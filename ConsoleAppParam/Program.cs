95218be1a0ffa1e7b004d6d2b018ac3d10826042
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:11:47 +0530

    Add project files.

---
+++ ConsoleAppParam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DemoParams7
+{
+    class Pocket
+    {
+        public int Multiply(params int[] Coins)
+        {
+            int total = 1;
+            foreach (int i in Coins)
+            {
+                total *= i;
+
+            }
+            return total;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pocket b1 = new Pocket();
+            Console.WriteLine("Total Coins In Pocket = " + b1.Multiply(5, 4));
+            Console.WriteLine("Total Coins In Pocket = " + b1.Multiply(5, 4, 6));
+            Console.WriteLine("Total Coins In Pocket = " + b1.Multiply(5, 4, 6, 40));
+            Console.Read();
+
+
+        }
+    }
+}
